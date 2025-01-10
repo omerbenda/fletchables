e31c1b906b0b623523f664deaa0628c1429b3761
omerbenda <omerbenda@gmail.com>
2025-01-10 14:09:08 +0200

    feat(FletchingScreen.java, FletchingScreenHandler.java): Add background image and inventory slots

--- src/client/java/com/fletchables/screens/FletchingScreen.java
+++ src/client/java/com/fletchables/screens/FletchingScreen.java
@@ -1,20 +1,24 @@
 package com.fletchables.screens;
 
+import com.fletchables.FletchablesMod;
 import com.fletchables.screenhandlers.FletchingScreenHandler;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class FletchingScreen extends HandledScreen<FletchingScreenHandler> {
+  private static final Identifier TEXTURE =
+      Identifier.of(FletchablesMod.MOD_ID, "textures/gui/container/fletching_table.png");
+
   public FletchingScreen(FletchingScreenHandler handler, PlayerInventory inventory, Text title) {
     super(handler, inventory, title);
   }
 
   @Override
   protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
-    context.drawItem(new ItemStack(Items.ARROW), 10, 10);
+    int yStart = (this.height - this.backgroundHeight) / 2;
+    context.drawTexture(TEXTURE, this.x, yStart, 0, 0, this.backgroundWidth, this.backgroundHeight);
   }
 }
--- src/main/java/com/fletchables/screenhandlers/FletchingScreenHandler.java
+++ src/main/java/com/fletchables/screenhandlers/FletchingScreenHandler.java
@@ -8,6 +8,7 @@ import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.slot.Slot;
 
 public class FletchingScreenHandler extends ScreenHandler {
   private final Inventory inventory;
@@ -23,6 +24,8 @@ public class FletchingScreenHandler extends ScreenHandler {
 
     this.inventory = inventory;
     this.context = context;
+
+    this.addInventorySlots();
   }
 
   @Override
@@ -33,5 +36,17 @@ public class FletchingScreenHandler extends ScreenHandler {
   @Override
   public boolean canUse(PlayerEntity player) {
     return ScreenHandler.canUse(this.context, player, Blocks.FLETCHING_TABLE);
+  }
+
+  private void addInventorySlots() {
+    for (int x = 0; x < 9; x++) {
+      this.addSlot(new Slot(this.inventory, x, 8 + x * 18, 142));
+    }
+
+    for (int y = 0; y < 3; y++) {
+      for (int x = 0; x < 9; x++) {
+        this.addSlot(new Slot(inventory, x + y * 9 + 9, 8 + x * 18, 84 + y * 18));
+      }
+    }
   }
 }
