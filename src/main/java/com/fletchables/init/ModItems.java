594345b7f147f8dd26b4b55077ec55f26449f0a9
omerbenda <omerbenda@gmail.com>
2025-01-17 16:13:45 +0200

    feat(ModItems.java): Add arrow items to combat creative tab

--- src/main/java/com/fletchables/init/ModItems.java
+++ src/main/java/com/fletchables/init/ModItems.java
@@ -3,7 +3,10 @@ package com.fletchables.init;
 import com.fletchables.FletchablesMod;
 import com.fletchables.items.EnderArrowItem;
 import com.fletchables.items.ExplosiveArrowItem;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,7 +17,14 @@ public class ModItems {
   public static final Item ENDER_ARROW =
       register(new EnderArrowItem(new Item.Settings()), "ender_arrow");
 
-  public static void initialize() {}
+  public static void initialize() {
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+        .register(
+            content -> {
+              content.addAfter(Items.SPECTRAL_ARROW, EXPLOSIVE_ARROW);
+              content.addAfter(EXPLOSIVE_ARROW, ENDER_ARROW);
+            });
+  }
 
   private static Item register(Item item, String id) {
     Identifier identifier = Identifier.of(FletchablesMod.MOD_ID, id);
